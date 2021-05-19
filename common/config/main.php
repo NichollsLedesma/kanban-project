a5e4576f356f0e4dc70a63c680ff981e2f3bab6b
Nico Ledesma <nicoledesma36@gmail.com>
2021-05-19 15:43:59 -0300

    clean and whole module adminLTE

--- common/config/main.php
+++ common/config/main.php
@@ -3,32 +3,11 @@ return [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
-        // '@nineinchnick/nfy' => '@vendor/nineinchnick/yii2-nfy', 
     ],
     'modules' => [
-		// 'nfy' => [
-		// 	'class' => 'nineinchnick\nfy\Module',
-		// ],
 	],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-        // 'dbmq' => [
-		// 	'class' => 'nineinchnick\nfy\components\DbQueue',
-		// 	'id' => 'queue',
-		// 	'label' => 'Notifications',
-		// 	'timeout' => 30,
-		// ],
-		// 'sysvmq' => [
-		// 	'class' => 'nineinchnick\nfy\components\SysVQueue',
-		// 	'id' => 'a',
-		// 	'label' => 'IPC queue',
-		// ],
-		// 'redismq' => [
-		// 	'class' => 'nineinchnick\nfy\components\RedisQueue',
-		// 	'id' => 'mq',
-		// 	'label' => 'Redis queue',
-		// 	'redis' => 'redis',
-		// ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
