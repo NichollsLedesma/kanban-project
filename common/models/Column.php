0b7988520d5353ca81a5590bded062912ec40802
Alejandro Zanlongo <azanlongo@gmail.com>
2021-05-26 16:03:52 -0300

    merge conflicts

---
+++ common/models/Column.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
+
+/**
+ * This is the model class for table "column".
+ *
+ * @property int $id
+ * @property string|null $uuid
+ * @property int $board_id
+ * @property int $owner_id
+ * @property string $title
+ * @property int $order
+ * @property int $created_by
+ * @property int $updated_by
+ * @property int $created_at
+ * @property int $updated_at
+ */
+class Column extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'column';
+    }
+
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::class,
+            BlameableBehavior::class
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            ['uuid', 'string', 'max' => 36],
+            ['uuid', 'unique'],
+            ['uuid', 'thamtech\uuid\validators\UuidValidator'],
+            [['board_id', 'owner_id', 'order', 'title'], 'required'],
+            [['board_id', 'owner_id', 'order', 'created_by', 'updated_by', 'created_at', 'updated_at'], 'integer'],
+            ['board_id', 'exist', 'targetClass' => Board::class, 'targetAttribute' => ['board_id' => 'id']],
+            ['owner_id', 'exist', 'targetClass' => User::class, 'targetAttribute' => ['owner_id' => 'id']],
+            [['title'], 'string', 'max' => 100],
+            ['title', 'match', 'pattern' => '/^[A-Za-z !.]{1,100}$/'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'uuid' => 'Uuid',
+            'board_id' => 'Board ID',
+            'owner_id' => 'Owner ID',
+            'title' => 'Title',
+            'order' => 'Order',
+            'created_by' => 'Created By',
+            'updated_by' => 'Updated By',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    public function getBoard()
+    {
+        return $this->hasOne(Board::class, ['id' => 'board_id']);
+    }
+
+    public function getCards()
+    {
+        return $this->hasMany(Card::class, ['card_id' => 'id']);
+    }
+
+    public function afterSave($insert, $changedAttributes)
+    {
+        if ($insert) {
+            $this->uuid = \thamtech\uuid\helpers\UuidHelper::uuid();
+            $this->save();
+        }
+
+        return true;
+    }
+}
---
+++ common/models/Entity.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
+
+/**
+ * This is the model class for table "entity".
+ *
+ * @property int $id
+ * @property string|null $uuid
+ * @property int $owner_id
+ * @property string $name
+ * @property int $created_by
+ * @property int $updated_by
+ * @property int $created_at
+ * @property int $updated_at
+ *
+ * @property UserEntity[] $userEntities
+ * @property User[] $users
+ */
+class Entity extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'entity';
+    }
+
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::class,
+            BlameableBehavior::class
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            ['uuid', 'string', 'max' => 36],
+            ['uuid', 'unique'],
+            ['uuid', 'thamtech\uuid\validators\UuidValidator'],
+            [['owner_id', 'name'], 'required'],
+            [['owner_id', 'created_by', 'updated_by', 'created_at', 'updated_at'], 'integer'],
+            ['owner_id', 'exist', 'targetClass' => User::class, 'targetAttribute' => ['owner_id' => 'id']],
+            [['name'], 'string', 'max' => 100],
+            ['name', 'match', 'pattern' => '/^[A-Za-z !.]{1,100}$/'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'uuid' => 'Uuid',
+            'owner_id' => 'Owner ID',
+            'name' => 'Name',
+            'created_by' => 'Created By',
+            'updated_by' => 'Updated By',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * Gets query for [[UserEntities]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserEntities()
+    {
+        return $this->hasMany(UserEntity::className(), ['entity_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Users]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUsers()
+    {
+        return $this->hasMany(User::className(), ['id' => 'user_id'])->viaTable('user_entity', ['entity_id' => 'id']);
+    }
+
+    public function getBoards()
+    {
+        return $this->hasMany(Board::class, ['entity_id' => 'id']);
+    }
+
+    public function afterSave($insert, $changedAttributes)
+    {
+        if ($insert) {
+            $this->uuid = \thamtech\uuid\helpers\UuidHelper::uuid();
+            $this->save();
+        }
+
+        return true;
+    }
+}
