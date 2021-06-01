2dd5907a8c80463aab96f1c5e093e55d56702ba0
Ricardo Cibrián <ricardo.cibrian@plandomino.com>
2021-06-01 18:46:31 -0500

    frontend/web/js: Remap draggable columns when Pjax is loaded

--- frontend/web/js/board-pjax.js
+++ frontend/web/js/board-pjax.js
@@ -1,13 +1,15 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 $(document).ready(function () {
-    $(".add-card").on("pjax:end", function () {
-        alert('si')
-        $.pjax.reload({container: "#board-container"});  //Reload GridView
+    $(document).on('pjax:end', function() {
+        const dragulaComp = dragula(
+            columns.map(column => document.getElementById(column))
+            );
+    	dragulaComp.containers.push(document.getElementById('board-body'));
     });
 });
 
